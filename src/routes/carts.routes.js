f05ea95b2bc8360eaf1a2011496aa2a258ea63f2
Lucas Berardi <lucas-l-@live.com>
2023-11-01 00:05:47 -0300

    Middlewares on carts.routes

--- src/routes/carts.routes.js
+++ src/routes/carts.routes.js
@@ -1,4 +1,5 @@
 import { Router } from "express"
+import { passportError, authorization } from "../utils/errorMessages.js"
 import * as cartController from "../controllers/cart.controller.js"
 
 const cartRouter = Router()
@@ -13,22 +14,22 @@ cartRouter.get('/:id', cartController.getCart)
 cartRouter.post('/', cartController.postCart)
 
 //Put cart with products array
-cartRouter.put('/:cid', cartController.putCartWithProdsArray)
+cartRouter.put('/:cid', passportError('jwt'), authorization('user'), cartController.putCartWithProdsArray)
 
 //Add product to cart
-cartRouter.put('/:cid/product/:pid', cartController.putProductToCart)
+cartRouter.put('/:cid/product/:pid', passportError('jwt'), authorization('user'), cartController.putProductToCart)
 
 //Put quantity of products on cart
-cartRouter.put('/:cid/products/:pid', cartController.putProdQty)
+cartRouter.put('/:cid/products/:pid', passportError('jwt'), authorization('user'), cartController.putProdQty)
 
 //Delete product on cart by id
-cartRouter.delete('/:cid/products/:pid', cartController.deleteProdOnCart)
+cartRouter.delete('/:cid/products/:pid', passportError('jwt'), authorization('user'), cartController.deleteProdOnCart)
 
 //Empty cart
-cartRouter.delete('/:id', cartController.emptyCart)
+cartRouter.delete('/:id', passportError('jwt'), authorization('user'), cartController.emptyCart)
 
 //Checkout - finalize purchase
-cartRouter.post('/:cid/purchase', cartController.purchase)
+cartRouter.post('/:cid/purchase', passportError('jwt'), authorization('user'), cartController.purchase)
 
 
 export default cartRouter
