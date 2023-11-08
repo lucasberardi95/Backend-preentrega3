3985aad7f1b1e94dcbbf475f1c8cf5558f0a48a8
Lucas Berardi <lucas-l-@live.com>
2023-11-08 01:28:46 -0300

    Mocking products with faker

--- src/routes/app.routes.js
+++ src/routes/app.routes.js
@@ -2,6 +2,7 @@ import { Router } from "express"
 import productRouter from "./products.routes.js"
 import cartRouter from "./carts.routes.js"
 import messageRouter from "./messages.routes.js"
+import routerMock from "./mocking.routes.js"
 import userRouter from './users.routes.js'
 import sessionRouter from './sessions.routes.js'
 import viewRouter from "./views.routes.js"
@@ -11,6 +12,7 @@ const router = Router()
 router.use('/api/products', productRouter)
 router.use('/api/carts', cartRouter)
 router.use('/api/messages', messageRouter)
+router.use('/api/mockingproducts', routerMock)
 router.use('/api/users', userRouter)
 router.use('/api/sessions', sessionRouter)
 router.use('/', viewRouter)
